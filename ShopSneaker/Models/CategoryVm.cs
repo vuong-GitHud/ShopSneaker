7012c54a0850d0a7ffbcb5e75269da97f2678033
Vuong Pham <vuongphamvc@gmail.com>
2023-11-05 16:55:40 +0700

    #

--- ShopSneaker/Models/CategoryVm.cs
+++ ShopSneaker/Models/CategoryVm.cs
@@ -8,6 +8,7 @@ namespace ShopSneaker.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public List<Product> Products { get; set; }
 
 
     }
