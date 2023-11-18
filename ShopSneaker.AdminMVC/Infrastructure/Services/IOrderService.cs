eb27f7420a05afb2878e198a323fafb29760fd85
Vuong Pham <vuongphamvc@gmail.com>
2023-11-18 17:10:54 +0700

    #

---
+++ ShopSneaker.AdminMVC/Infrastructure/Services/IOrderService.cs
@@ -0,0 +1,8 @@
+﻿using ShopSneaker.AdminMVC.Model.Dashboard;
+
+namespace ShopSneaker.AdminMVC.Infrastructure.Services;
+
+public interface IOrderService
+{
+    Task<OrderViewModel> GetOrder();
+}
---
+++ ShopSneaker.AdminMVC/Infrastructure/Services/OrderService.cs
@@ -0,0 +1,35 @@
+﻿using ShopSneaker.AdminMVC.Model.Dashboard;
+using ShopSneaker.Areas.Identity.Data;
+
+namespace ShopSneaker.AdminMVC.Infrastructure.Services;
+
+public class OrderService : IOrderService
+{
+    private readonly ShopSneakerContext _context;
+
+    public OrderService(ShopSneakerContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<OrderViewModel> GetOrder()
+    {
+        var order = _context.Orders.OrderByDescending(o => o.OrderDate);
+        if (order.Count() < 1)
+        {
+            return new OrderViewModel();
+        }
+        var monthly = DateTime.Now.AddDays(-30);
+        var now = DateTime.Now;
+        var currentYear = now.Year;
+        var totalMonth = order.Where(o => o.OrderDate >= monthly && o.OrderDate <= now);
+        var revenue = order.Where(o => o.isPayment && o.OrderDate >= monthly && o.OrderDate <= now);
+
+        return new OrderViewModel()
+        {
+            TotalOrderMonth = totalMonth.Count(),
+            TotalRevenue = revenue.Sum(r => r.Amount),
+            Orders = order.Take(5).ToList(),
+        };
+    }
+}
